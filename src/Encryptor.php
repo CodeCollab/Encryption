62a7425f9ed7bfd7db10fe4796e0ed8c5c867d8f
PeeHaa <info@pieterhordijk.com>
2015-08-02 19:46:36 +0200

    Added encryptor interface

---
+++ src/Encryptor.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * Interface for encryption classes
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Encryption
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Security\Encryption;
+
+/**
+ * Interface for encryption classes
+ *
+ * @category   CodeCollab
+ * @package    Encryption
+ * @author     Pieter Hordijk <info@pieterhordijk.com
+ */
+interface Encryptor
+{
+    /**
+     * Encrypts data
+     *
+     * @param string $data The data to encrypt
+     *
+     * @return string The encrypted data
+     */
+    public function encrypt(string $data): string;
+
+    /**
+     * Decrypts data
+     *
+     * @param string $data The data to decrypt
+     *
+     * @return string The decrypted data
+     */
+    public function decrypt(string $data): string;
+}
