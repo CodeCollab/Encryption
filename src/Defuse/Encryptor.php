eeed3f78b63bb6afabd06b08fb0e270ff965f8d9
PeeHaa <info@pieterhordijk.com>
2015-08-29 03:03:52 +0200

    Implemented defuse encryptor

---
+++ src/Defuse/Encryptor.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+/**
+ * Defuse encryptor implementation
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Encryption
+ * @subpackage Defuse
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Encryption\Defuse;
+
+use CodeCollab\Encryption\Encryptor as EncryptorInterface;
+use Crypto;
+use CodeCollab\Encryption\CryptoException;
+
+/**
+ * Defuse encryptor implementation
+ *
+ * @category   CodeCollab
+ * @package    Encryption
+ * @subpackage Defuse
+ * @author     Pieter Hordijk <info@pieterhordijk.com
+ */
+class Encryptor implements EncryptorInterface
+{
+    /**
+     * @var string The encryption key
+     */
+    private $key;
+
+    /**
+     * Creates instance
+     *
+     * @param string $key The encryption key
+     */
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * Encrypts data
+     *
+     * @param string $data The data to encrypt
+     *
+     * @return string The encrypted data
+     */
+    public function encrypt(string $data): string
+    {
+        try {
+            return Crypto::encrypt($data, $this->key);
+        } catch(\Exception $e) {
+            throw new CryptoException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
---
+++ test/Unit/Defuse/EncryptorTest.php
@@ -0,0 +1,175 @@
+<?php declare(strict_types=1);
+
+namespace CodeCollabTest\Unit\Encryption\Defuse;
+
+use CodeCollab\Encryption\Defuse\Encryptor;
+use CodeCollab\Encryption\Defuse\Key;
+
+class EncryptorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     */
+    public function testImplementsCorrectInterface()
+    {
+        $encryptor = new Encryptor((new Key())->generate());
+
+        $this->assertInstanceOf('CodeCollab\Encryption\Encryptor', $encryptor);
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptSuccess()
+    {
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $this->assertSame(64, strlen($encryptor->encrypt('foobarbaz')));
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnBadKey()
+    {
+        $this->setExpectedException('CodeCollab\Encryption\CryptoException');
+
+        $encryptor = new Encryptor('bad key');
+
+        $encryptor->encrypt('foobarbaz');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnOpensslGetCipherMethodsNotAvailable()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_get_cipher_methods');
+        uopz_delete('openssl_get_cipher_methods');
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $encryptor->encrypt('foobarbaz');
+
+        uopz_restore('openssl_get_cipher_methods');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnOpensslGetCipherMethodsCipherMethodNotAvailable()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_get_cipher_methods');
+        uopz_function('openssl_get_cipher_methods', function(bool $aliases = false): array {
+            return [];
+        });
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $encryptor->encrypt('foobarbaz');
+
+        uopz_restore('openssl_get_cipher_methods');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnOpensslCipherIvLengthMethodNotAvailable()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_cipher_iv_length');
+        uopz_delete('openssl_cipher_iv_length');
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $encryptor->encrypt('foobarbaz');
+
+        uopz_restore('openssl_cipher_iv_length');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnOpensslCipherIvLengthReturnsFalse()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_cipher_iv_length');
+        uopz_function('openssl_cipher_iv_length', function(string $method): int {
+            return false;
+        });
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $encryptor->encrypt('foobarbaz');
+
+        uopz_restore('openssl_cipher_iv_length');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Encryptor::encrypt
+     */
+    public function testEncryptThrowsOnOpensslCipherIvLengthReturnsIvSizeSmallerThanZero()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_cipher_iv_length');
+        uopz_function('openssl_cipher_iv_length', function(string $method): int {
+            return -1;
+        });
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $encryptor = new Encryptor($key);
+
+        $encryptor->encrypt('foobarbaz');
+
+        uopz_restore('openssl_cipher_iv_length');
+    }
+}
