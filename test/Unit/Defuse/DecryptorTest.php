692228991dc8c880221457f8c6eb68d79ba6e59b
PeeHaa <info@pieterhordijk.com>
2015-08-31 17:24:46 +0200

    Added more decryptor tests

--- test/Unit/Defuse/DecryptorTest.php
+++ test/Unit/Defuse/DecryptorTest.php
@@ -45,4 +45,76 @@ class DecryptorTest extends \PHPUnit_Framework_TestCase
 
         $decryptor->decrypt($encryptor->encrypt('foobarbaz'));
     }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::decrypt
+     */
+    public function testDecryptThrowsOnOpensslGetCipherMethodsNotAvailable()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_get_cipher_methods');
+        uopz_delete('openssl_get_cipher_methods');
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $decryptor = new Decryptor($key);
+
+        $decryptor->decrypt('ciphertext');
+
+        uopz_restore('openssl_get_cipher_methods');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::decrypt
+     */
+    public function testDecryptThrowsOnOpensslGetCipherMethodsCipherMethodNotAvailable()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        uopz_backup('openssl_get_cipher_methods');
+        uopz_function('openssl_get_cipher_methods', function(bool $aliases = false): array {
+            return [];
+        });
+
+        $this->setExpectedException('CodeCollabLib\Encryption\CryptoException');
+
+        $key       = (new Key())->generate();
+        $decryptor = new Decryptor($key);
+
+        $decryptor->encrypt('ciphertext');
+
+        uopz_restore('openssl_get_cipher_methods');
+    }
+
+    /**
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::__construct
+     * @covers CodeCollab\Encryption\Defuse\Decryptor::decrypt
+     */
+    public function testDecryptThrowsOnNonValidCipherTextLength()
+    {
+        if (!function_exists('uopz_backup')) {
+            $this->markTestSkipped('uopz extension is not installed.');
+
+            return;
+        }
+
+        $this->setExpectedException('CodeCollabLib\Encryption\FraudException');
+
+        $key       = (new Key())->generate();
+        $decryptor = new Decryptor($key);
+
+        $decryptor->encrypt('ciphertext');
+    }
 }
